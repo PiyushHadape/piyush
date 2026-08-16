20088351dbb37e4abbcb3ea9ebdbc0fe155b294e
PiyushHadape <piyushhadape9@gmail.com>
2026-08-16 23:51:45 +0530

    added

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ alert("Hello, World!");
 console.log("This is a log message.");
 let name = prompt("Please enter your name:");
 console.log("Hello, " + name + "!");
+console.log("This is a test message.");
