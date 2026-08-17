34f65ea0d1eabce8310e33117b754d0d4f994747
PiyushHadape <piyushhadape9@gmail.com>
2026-08-17 22:18:48 +0530

    added

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ console.log("This is a log message.");
 let name = prompt("Please enter your name:");
 console.log("Hello, " + name + "!");
 console.log("This is a test message.");
+let a=5;
+let b=10;
+let sum=a+b;
+console.log("The sum of " + a + " and " + b + " is: " + sum);
